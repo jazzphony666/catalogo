f41d5e35d0cebe183988bca4572d4b24036fb495
jazzphony666 <146792724+jazzphony666@users.noreply.github.com>
2025-11-16 18:38:02 -0600

    Set up Firebase initialization in main
    Initialize Firebase in the main function.

--- main.dart
+++ main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'movie.dart'; //
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(const CatalogoPeliculasApp());
 }
 
